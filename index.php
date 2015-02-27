db8afe3537315ae59c88608eba06af5114fa756b
Philippe Traverse <philippe.traverse@gmail.com>
2015-02-27 09:12:30 +0000

    Init Commit Initially Committed

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+echo '<h1>Hi Camille</h1>
+
+<a href="http://ec2-52-1-245-195.compute-1.amazonaws.com/">http://ec2-52-1-245-195.compute-1.amazonaws.com/</a>
+<br>
+<br>
+This is where the site will be until its finished
+<br>
+';
+
+$i = 0;
+while ($i<3)
+{
+	echo "PHP-Test $i <br>";
+	$i++;
+}
