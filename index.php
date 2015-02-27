56ba630f2996f84ba780a7f3476d9a8b494ce13e
Philippe Traverse <philippe.traverse@gmail.com>
2015-02-27 02:14:12 -0800

    added foundation and icons etc

--- index.php
+++ index.php
@@ -1,17 +1,113 @@
 <?php
 
-echo '<h1>Hi Camille</h1>
+?>
 
-<a href="http://ec2-52-1-245-195.compute-1.amazonaws.com/">http://ec2-52-1-245-195.compute-1.amazonaws.com/</a>
-<br>
-<br>
-This is where the site will be until its finished
-<br>
-';
+<!DOCTYPE html>
+<html>
+<head>
+<title>Camilles Traverse is so awesome!!</title>
 
-$i = 0;
-while ($i<3)
-{
-	echo "PHP-Test $i <br>";
-	$i++;
-}
+<link href="css/normalize.css" type="text/css" rel="stylesheet"/>
+<link href="css/foundation.css" type="text/css" rel="stylesheet"/>
+<script src="/js/vendor/modernizr.js"></script>
+
+</head>
+
+	
+<body>
+
+      <div class="row">
+        <div class="large-3 columns">
+          <h1>
+          	<img src="img/flags/gif/ca.gif"/>
+          	<img src="img/flags/gif/fr.gif"/>
+          	<img src="img/flags/gif/cl.gif"/>
+          </h1>
+        </div>
+        <div class="large-9 columns" style="padding-top:10px;">
+          <ul class="right button-group">
+          <li><a href="/" class="button">Home</a></li>
+          <li><a href="/about" class="button">About</a></li>
+          <li><a href="/contact" class="button">Contact</a></li>
+          <li><a href="/services" class="button">Services</a></li>
+          <li><a href="/funding" class="button">Funding</a></li>
+          </ul>
+         </div>
+       </div>
+      
+     
+     
+     
+     
+      <div class="row">
+        <div class="large-12 columns">
+        <div id="slider">
+          <img src="http://placehold.it/1000x400&text=I ama speecheth Rapist"/>
+        </div>
+        
+        <hr/>
+        </div>
+      </div>
+      
+     
+     
+      <div class="row">
+        <div class="large-4 columns">
+          <img src="http://placehold.it/400x300&text=[More]"/>
+          <h4>I can help you talk gooder.</h4>
+          <p>Bacon coca cola cheeseburger pizza coffee.</p>
+        </div>
+        
+        <div class="large-4 columns">
+          <img src="http://placehold.it/400x300&text=[Stuff]"/>
+          <h4>This is a content section.</h4>
+          <p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
+        </div>
+        
+        <div class="large-4 columns">
+          <img src="http://placehold.it/400x300&text=[Here]"/>
+          <h4>This is a content section.</h4>
+          <p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
+        </div>
+      
+        </div>
+        
+     
+    <div class="row">
+        <div class="large-12 columns">
+        
+          <div class="panel">
+            <h4>Get in touch!</h4>
+                
+            <div class="row">
+              <div class="large-9 columns">
+                <p>Wed love to hear from you, you attractive person you.</p>
+              </div>
+              <div class="large-3 columns">
+                <a href="#" class="radius button right">Contact Us</a>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+     
+       
+      
+      <footer class="row">
+      	<hr>
+        <div class="large-3 large-centered columns">          
+          	<div>
+          		Footer Stuff Can Happen Here
+          	</div>
+        </div> 
+      </footer>
+    
+
+<script src="/js/vendor/jquery.js"></script> 
+<script src="/js/vendor/fastclick.js"></script>
+<script src="js/foundation/foundation.js"></script>
+<script> 
+	$(document).foundation(); 
+</script>
+</body>
